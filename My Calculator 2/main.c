f3e281c63ca9b9d5383652fe936e524c911ebfce
Edward Barber <luckyshot423@gmail.com>
2016-08-09 20:18:23 -0400

    Here is my updated calculator, please don't look at the old one K THX.

--- My Calculator 2/main.c
+++ My Calculator 2/main.c
@@ -9,7 +9,41 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int numOne = 7;
+    int numTwo = 7;
+    int calcEh = 7;
+    int pam = 7;
+    
+    printf("What is your first number?: ");
+    scanf("%d", &numOne);
+    
+    printf("What is your second number?: ");
+    scanf("%d", &numTwo);
+    
+    printf("Addition: 0\nSubtraction: 1\nMultiplication: 2\nDivision: 3\n\nHow do you want to calculate?: \n");
+    while(calcEh < 0 || calcEh > 3) {
+        fpurge(stdin);
+        scanf("%d", &calcEh);
+    }
+    
+    if (calcEh == 0) {
+        (pam = numOne + numTwo);
+        printf("Your number is... %d \n", pam);
+    }
+    if (calcEh == 1) {
+        (pam = numOne - numTwo);
+        printf("Your number is... %d \n", pam);
+    }
+    if (calcEh == 2) {
+        (pam = numOne * numTwo);
+        printf("Your number is... %d \n", pam);
+    }
+    if (calcEh == 3) {
+        (pam = numOne / numTwo);
+        printf("Why did you do that...? %d \n", pam);
+    }
+    
+    
     return 0;
 }
