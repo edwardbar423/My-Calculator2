5e122f47e1067d5b4f6128ee7faaed94cf94ef68
Edward Barber <luckyshot423@gmail.com>
2016-08-09 20:15:13 -0400

    Initial Commit

---
+++ My Calculator 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  My Calculator 2
+//
+//  Created by Andrew Barber on 8/9/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
